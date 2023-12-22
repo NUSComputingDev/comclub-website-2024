317ec951a3b9d2db5d2e180301fc8b36717d77cb
Respirayson <raysonyeap4c@gmail.com>
2023-12-22 17:49:22 +0800

    Fix config file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/comclub-website-2024/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
