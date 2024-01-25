864ce6510cc32b2177d7666d15b901e5d62ec398
Leslie Yip <leslieyip02@gmail.com>
2024-01-25 11:35:43 +0800

    Change phrasing of subheading

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -51,7 +51,7 @@ function Footer() {
           NUS Students&#39; Computing Club
         </h4>
         <p className='font-subheading text-sm'>
-          A constituent of the NUS Student Union
+          A constituent club of the NUS Students' Union
         </p>
 
         <div className='flex flex-row flex-wrap'>
